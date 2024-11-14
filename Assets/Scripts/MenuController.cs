9175a421dd5f354c5427d711aad5135e7e555f97
DexBrn <getdex555@gmail.com>
2024-11-14 15:37:22 +0000

    Continued on menu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,6 +9,8 @@ public class MenuController : MonoBehaviour
 {
     public Transform MenuPanel;
     Image MenuPanelMain;
+    public Transform AbilityPanel;
+    string AbilityToSwitch; 
 
     // Start is called before the first frame update
     void Start()
@@ -23,13 +26,44 @@ public class MenuController : MonoBehaviour
         {
             MenuPanel.gameObject.SetActive(true);
             MenuPanelMain.enabled = true;
+            Cursor.lockState = CursorLockMode.None;
         }
         else if (Input.GetKeyDown(KeyCode.Escape) && MenuPanelMain.enabled == true)
         {
             MenuPanel.gameObject.SetActive(false);
             MenuPanelMain.enabled = false;
+            Cursor.lockState = CursorLockMode.Locked;
         }
 
 
+        
+
+
+
     }
+
+    public void AbilityPanelState()
+    {
+        if (AbilityPanel.gameObject.active == false) 
+        { 
+            AbilityPanel.gameObject.SetActive(true);
+
+        
+        }
+        else
+        {
+            AbilityPanel.gameObject.SetActive(false);
+            AbilityToSwitch = AbilityPanel.GetChild(0).GetComponent<TextMeshPro>().text;
+            MenuPanel.GetChild(2).GetComponent<TextMeshPro>().text = AbilityToSwitch;
+        }
+    }
+
+
+
+
+
+
+
+
+
 }
